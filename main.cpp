afaabe7316675da51ca81d695d476ca03ed3c54d
126Florian <91480650+126Florian@users.noreply.github.com>
2021-12-01 16:03:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+using namespace std;
+#include "lib.h"
 int main(){
+  char a=a;
+  cin >> a;
+  if (conversione (a)==false){
+     cout <<"errore";
+  }endl{
+  cout<<a;
+  }
   return 0;
 }
